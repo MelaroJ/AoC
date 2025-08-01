2a486ce95ebef43456d5c0f4ae6c7a35fcddd408
Jake Melaro <melarojake@gmail.com>
2025-08-01 12:03:30 -0400

    feat(rust/day01): cargo project, finished day01. input files kept in day## dirs and src/day##.rs called by src/main.rs

--- 2024/rust/day01/main.rs
+++
@@ -1,6 +0,0 @@
-use std::fs;
-
-fn main() {
-    let input = fs::read_to_string("input").unwrap();
-    println!("{}", input);
-}
---
+++ 2024/rust/src/day01.rs
@@ -0,0 +1,7 @@
+use std::fs;
+
+pub fn solve() {
+    let input = fs::read_to_string("day01/input").unwrap();
+    println!("{}", input);
+}
+

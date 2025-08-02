73cf9ea9a76aea2fa184133313829ca19d34f83d
Jake Melaro <melarojake@gmail.com>
2025-08-01 22:22:37 -0400

    [feat](r/day01) - what a pain getting air formatter setup

--- 2024/r/day01/main.R
+++ 2024/r/day01/main.R
@@ -1,2 +1,20 @@
-data <- readLines("input")
-print(data)
+data <- read.delim("input", header = FALSE, sep = "", col.names = c("left", "right")) # nolint: line_length_linter.
+
+data |>
+  (\(df) {
+    left <- sort(df$left)
+    right <- sort(df$right)
+    part1 <- sum(abs(left - right))
+    message(part1)
+  })()
+
+data |>
+  (\(df) {
+    left_vals <- df$left
+    right_counts <- table(df$right)
+    part2 <- sum(sapply(left_vals, function(x) {
+      count <- right_counts[as.character(x)]
+      x * ifelse(is.na(count), 0, count)
+    }))
+    message(part2)
+  })()

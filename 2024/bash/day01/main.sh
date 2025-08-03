cf2486d9837de1a539685f37457141b47a3ec5cd
Jake Melaro <melarojake@gmail.com>
2025-08-02 20:39:52 -0400

    [feat](bash/01) - shaky at best

--- 2024/bash/day01/main.sh
+++ 2024/bash/day01/main.sh
@@ -1,3 +1,44 @@
 #!/usr/bin/env bash
 
-cat input
+left=()
+right=()
+
+while read -r l r; do
+  left+=("$l")
+  right+=("$r")
+done
+
+mapfile -t leftsorted < <(printf '%d\n' "${left[@]}" | sort -n)
+mapfile -t rightsorted < <(printf '%d\n' "${right[@]}" | sort -n)
+
+len=${#left[@]}
+
+p1=0
+for ((i = 0; i < len; i++)); do
+  x=${leftsorted[$i]}
+  y=${rightsorted[$i]}
+
+  diff=$((x - y))
+
+  #abs
+  dist=${diff#-}
+
+  ((p1 += dist))
+done
+
+echo "$p1"
+
+#freq right array
+declare -A r_count
+for value in "${right[@]}"; do
+  ((r_count[$value]++))
+done
+
+p2=0
+for value in "${left[@]}"; do
+  freq=${r_count[$value]}
+  prod=$((value * freq))
+  ((p2 += prod))
+done
+
+echo "$p2"
